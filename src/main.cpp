faef2ea2fd3427c3e7ea88fd767ba2d0a60670f9
Randy Chapman <wilbur4321@gmail.com>
2026-08-26 19:18:21 -0700

    WiFi support

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,27 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include "secrets.h"
 
-const char* WIFI_SSID = "your-ssid";
-const char* WIFI_PASSWORD = "your-password";
+void setup()
+{
+    Serial.begin(115200);
 
-void setup() {
-  Serial.begin(115200);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
 
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+    Serial.println();
+    Serial.print("Connected, IP address: ");
+    Serial.println(WiFi.localIP());
 
-  Serial.println();
-  Serial.print("Connected, IP address: ");
-  Serial.println(WiFi.localIP());
-
-  Serial.println("Hello world");
+    Serial.println("Hello world");
 }
 
-void loop() {
+void loop()
+{
+    Serial.println("Hello world from loop");
+    delay(1000);
 }
